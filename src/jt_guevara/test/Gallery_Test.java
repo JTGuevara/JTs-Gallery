12890298d2ede42bf53e586e8154dcf383d1ac96
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-12 18:47:39 -0400

    Updated Gallery_Test.java (unit tests)
    - Submitted unit tests for default constructor, Gallery(), and getImages(). Documented both functions
    - Modified function headers to have their proper return types

--- src/jt_guevara/test/Gallery_Test.java
+++ src/jt_guevara/test/Gallery_Test.java
@@ -1,6 +1,14 @@
 /* UNIT TESTS FOR GALLERY.JAVA
  * 
  * TESTING FRAMEWORK USED: JUNIT 5
+ *
+ * FUNCTIONS:
+ *      public testGallery();
+ *          RESULT: Verifies that the Gallery constructor creates a valid object and its private member, images,
+ *                  is assigned an ArrayDeque value
+ * 
+ *      public ArrayDeque<Image> testGetImages();
+ *          RESULT: Verifies that a valid ArrayDeque with a valid size is returned, and then returns it
  */
 
 package jt_guevara.test;
@@ -10,36 +18,49 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayDeque;
 import javafx.scene.image.Image;
 
-class Gallery_Test {
+public class Gallery_Test {
 
+	//TEST OF DEFAULT AND VALUE CONSTRUCTORS (Gallery(), Gallery(int size))
 	@Test
-	void testGallery() {
-		
+	public void testGallery() {
+		Gallery gallery = new Gallery();
+		ArrayDeque<Image> images = new ArrayDeque<Image>();
+		assertNotNull(gallery);
+		assertNotNull(images);
 	}
 
 	@Test
-	void testGalleryInt() {
-		
+	public void testGalleryInt() {
+		//to do later
+	}
+	
+	//TEST OF MODIFICATION FUNCTIONS (Gallery.addImage(), Gallery.clearGallery())
+	@Test
+	public void testAddImage() {
+		//to do later
+	}
+	
+	@Test
+	public void testClearGallery() {
+		//to do later
+	}
+
+	//TEST OF RETURN FUNCTIONS (Gallery.getImages(), Gallery.getSize())
+	@Test
+	public ArrayDeque<Image> testGetImages() {
+		int size = 0;
+		ArrayDeque<Image> images = new ArrayDeque<Image>();
+		//check for valid size
+		if(size < 0)
+		    fail();
+		assertNotNull(images);
+		assertEquals(size,0);
+		return images;
 	}
 
 	@Test
-	void testGetImages() {
-		
-	}
-
-	@Test
-	void testAddImage() {
-		
-	}
-
-	@Test
-	void testGetSize() {
-		
-	}
-
-	@Test
-	void testClearGallery() {
-		
+	public int testGetSize() {
+		//to do later
 	}
 
 }
