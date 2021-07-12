02cb4043f3129247564d69cd314ccfa08b79677b
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-11 23:11:56 -0400

    Uploaded 'test' folder and Gallery_Test.java
    Uploaded 'test' folder and unit test for Gallery.java class

---
+++ src/jt_guevara/test/Gallery_Test.java
@@ -0,0 +1,45 @@
+/* UNIT TESTS FOR GALLERY.JAVA
+ * 
+ * TESTING FRAMEWORK USED: JUNIT 5
+ */
+
+package jt_guevara.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayDeque;
+import javafx.scene.image.Image;
+
+class Gallery_Test {
+
+	@Test
+	void testGallery() {
+		
+	}
+
+	@Test
+	void testGalleryInt() {
+		
+	}
+
+	@Test
+	void testGetImages() {
+		
+	}
+
+	@Test
+	void testAddImage() {
+		
+	}
+
+	@Test
+	void testGetSize() {
+		
+	}
+
+	@Test
+	void testClearGallery() {
+		
+	}
+
+}
