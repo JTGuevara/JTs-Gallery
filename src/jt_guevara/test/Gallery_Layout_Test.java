0c8493c89dda735d495d0cd1a63aac6892e001e1
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-19 23:59:38 -0400

    Created Gallery_Layout_Test.java
    Created Gallery_Layout_Test.java for unit tests of Gallery_Layout class

---
+++ src/jt_guevara/test/Gallery_Layout_Test.java
@@ -0,0 +1,18 @@
+package jt_guevara.test;
+
+import org.junit.jupiter.api.Test;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class Gallery_Layout_Test extends Application{
+
+	@Test
+	public void Generate_Gallery_Test() {
+		
+	}
+
+	@Override
+	public void start(Stage window) throws Exception {
+		
+	}
+}
