64a81ca22aeef194c432221b79d4ceec35183c18
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-20 21:56:09 -0400

    Updated Gallery_Layout_Test.java (unit test)
    Submitted unit test for portion Gallery_Layout.set_gallery_appearance() function

--- src/jt_guevara/test/Gallery_Layout_Test.java
+++ src/jt_guevara/test/Gallery_Layout_Test.java
@@ -1,7 +1,10 @@
 package jt_guevara.test;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 class Gallery_Layout_Test extends Application{
@@ -9,6 +12,13 @@ class Gallery_Layout_Test extends Application{
 	@Test
 	public void Generate_Gallery_Test() {
 		
+	}
+	
+	@Test
+	public void set_gallery_appearance_alignment_test() {
+		GridPane p = new GridPane();
+		p.setAlignment(Pos.CENTER);
+		assertEquals(p.getAlignment(), Pos.CENTER);
 	}
 
 	@Override
