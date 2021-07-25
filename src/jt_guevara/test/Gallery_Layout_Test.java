cbff11f3ecddba39bdd2391a9872f3f0c4a8b616
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-24 21:48:17 -0400

    Updated Gallery_Layout_Test.java (unit tests)
    Uploaded unit tests for create_image_canvas() and bind_gallery(). Modified generate_gallery() test

--- src/jt_guevara/test/Gallery_Layout_Test.java
+++ src/jt_guevara/test/Gallery_Layout_Test.java
@@ -1,6 +1,6 @@
 /*UNIT TESTS FOR GALLERY_LAYOUT.JAVA
  * FILE: Gallery_Layout_Test.java
- * DESCRIPTION: Unit test for Window_Layout.java class(generate_gallery() and private functions)
+ * DESCRIPTION: Unit test for Window_Layout.java class(generate_gallery() function and private functions)
  * 
  * FUNCTIONS:
  *   public void generate_gallery_test();
@@ -11,11 +11,18 @@
  *     
  *   public void set_gallery_appearance_height_test();
  *     RESULT: Verifies that the gallery layout has the correct minimum and maximum height
+ *     
+ *   public void create_image_canvas_test();
+ *     RESULT: Verifies that an image view is binded to its canvas
+ *   
+ *   public void bind_gallery_test();
+ *     RESULT: Verifies that a canvas is binded to the gallery
  *   
  */
 package jt_guevara.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
@@ -23,6 +30,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -30,17 +38,21 @@ import jt_guevara.Gallery_Layout;
 
 
 public class Gallery_Layout_Test extends Application{
-
+	//generate_gallery() test
 	@Test
 	public void generate_gallery_test() {
 		int expectedCanvases = 3;
 		GridPane galleryLayout = new GridPane();
-		StackPane[] canvases = new StackPane[3];
-		galleryLayout.getChildren().addAll(canvases);
-		assertEquals(expectedCanvases, canvases.length);
-		assertEquals(galleryLayout.getChildren().size(), 3);
+		StackPane canvas1 = new StackPane();
+		StackPane canvas2 = new StackPane();
+		StackPane canvas3 = new StackPane();
+		galleryLayout.getChildren().addAll(canvas1, canvas2, canvas3);
+		if(expectedCanvases != 3)
+			fail();
+		assertEquals(expectedCanvases, galleryLayout.getChildren().size());//check the layout for the expected number of canvases
 	}
 	
+	//set_gallery_appearance() test
 	@Test
 	public void set_gallery_appearance_alignment_test() {
 		GridPane layout = new GridPane();
@@ -56,6 +68,28 @@ public class Gallery_Layout_Test extends Application{
 		assertEquals(layout.getMinHeight(), 250, 0.001);
 		assertEquals(layout.getMaxHeight(), 700, 0.001);
 	}
+	
+	//create_image_canvas() test
+	@Test
+	public StackPane create_image_canvas_test() {
+		StackPane canvas = new StackPane();
+		ImageView imgView = new ImageView();
+		imgView.fitHeightProperty().bind(canvas.heightProperty().subtract(25));
+		imgView.fitWidthProperty().bind(canvas.widthProperty().subtract(25));
+		assertEquals(imgView.fitHeightProperty().isBound(), true);
+		assertEquals(imgView.fitWidthProperty().isBound(), true);
+		return canvas;
+	}
+	
+	//bind_gallery() test
+	@Test
+	public void bind_gallery_test() {
+		GridPane galleryLayout = new GridPane();
+		StackPane canvas = new StackPane();
+		canvas.prefWidthProperty().bind(galleryLayout.widthProperty());
+		assertEquals(canvas.prefWidthProperty().isBound(), true);
+	}
+	
 
 	@Override
 	public void start(Stage window) throws Exception {
@@ -64,12 +98,11 @@ public class Gallery_Layout_Test extends Application{
 		Gallery_Layout.generate_gallery(window, p);
 		window.setScene(s);
 		window.show();
-		//Platform.exit();	//<--disable as needed to see the intended output
+		Platform.exit();	//<--disable as needed to see the intended output
 	}
 	
 	@AfterAll
-	public static void launchFX() {
+	public static void launchFX() { //launches JavaFX application (launches after tests)
 		Application.launch();
 	}
 }
-
