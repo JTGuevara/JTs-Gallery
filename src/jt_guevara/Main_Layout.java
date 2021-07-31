f60b0e8609411ff9babc452cc3279720e80d2350
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-31 15:11:55 -0400

    Modified Main_Layout.java
    - Added set_menu_items() function to specifically handle menu-related tasks
    - Transferred menu tasks from the old setWindow() function to set_menu_items()

--- src/jt_guevara/Main_Layout.java
+++ src/jt_guevara/Main_Layout.java
@@ -32,9 +32,6 @@ public class Main_Layout {
 	public GridPane getLayout() {return layout;}
 	public static void setWindow(Stage window, GridPane p)
 	{
-		//Declare menu items to be used as clickable text for the user
-		Text load_gallery = new Text("Load Gallery    ");
-		Text exit = new Text("Exit");
 		//Set menu item and layout appearance and properties
 		p.setStyle("-fx-background-color: black;-fx-border-color: blue");
 		p.setPadding(new Insets(10,10,10,10));
@@ -42,14 +39,18 @@ public class Main_Layout {
 		window.setTitle("JT's Gallery");
 		window.setMinHeight(600);
 		window.setMinWidth(800);
+	}
+	
+	public void set_menu_items(){
+		//Declare menu items to be used as clickable text for the user
+		Text load_gallery = new Text("Load Gallery");
+		Text exit = new Text("Exit");
 		load_gallery.setFill(Color.WHITE);
 		exit.setFill(Color.WHITE);
-		//Add menu items to window via main layout(GridPane p)
-		//horizontal box used to store and group menu items
+		//Menu bar to store menu items
 		HBox menuBar = new HBox();
 		menuBar.getChildren().add(load_gallery);
 		menuBar.getChildren().add(exit);
 		p.add(menuBar, 0, 0);
-		
 	}
 }
