416a041383e3ddd375ed669d45f8d633cba667e5
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-31 19:27:22 -0400

    Modified Gallery_Display.java
    - Added new class and function info
    - Added new data fields: GridPane display, StackPane leftCanvas, StackPane midCanvas, StackPane rightCanvas
    - Added constructor

--- src/jt_guevara/Gallery_Display.java
+++ src/jt_guevara/Gallery_Display.java
@@ -1,4 +1,48 @@
-
+/* FILE: Gallery_Display.java
+ * CLASS DESCRIPTION: The Gallery_Display class is the visual representation of the image gallery. A Gallery_Display consists of four components: 
+ *                   a display component(or wall) for holding image canvases, a left canvas, a middle canvas and a right canvas	
+ * 
+ * FUNCTIONS:
+ *    public Gallery_Display();
+ *        DESCRIPTION: Declares an empty Gallery_Display by initializing a display(GridPane)
+ *        
+ *        
+ *    public GridPane getDisplay();
+ *        DESCRIPTION: Returns the display
+ *        
+ *        
+ *    public void setup_gallery();
+ *        DESCRIPTION: Sets up gallery by setting style properties to the gallery display, setting style properties for each image canvas, 
+ *                     applying an image view to each canvas as well as adding all canvases to the display. To do this, setup_gallery() calls 
+ *                     the following private functions to accomplish its task: set_image_canvas(), set_image_view(), add_image_canvases() 
+ *                     
+ *                     
+ *    private void set_gallery_style();
+ *        DESCRIPTION: The gallery display has its style properties set
+ *        
+ *        
+ *    private void set_image_canvas(StackPane canvas);
+ *        PARAMETER: StackPane canvas - image canvas used to set style properties
+ *        DESCRIPTION: Modifies and styles the image canvas
+ *        
+ *        
+ *    private void set_image_view(StackPane canvas);
+ *        PARAMETER: StackPane canvas - image canvas used for adding an image view component
+ *        DESCRIPTION: Initializes an image view component(ImageView) and sets its properties. The image view is then binded and added
+ *                     to the canvas.
+ *    
+ *    
+ *    private void add_image_canvases();
+ *        DESCRIPTION: Adds image canvases to the display
+ *        
+ *        
+ *    public void bind_gallery(Stage window);
+ *        PARAMETER: Stage window - application window used to apply property binding
+ *        DESCRIPTION: This function binds(synchronizes) width and height properties of the gallery display to width and height properties of
+ *                     the application window so that the gallery display is automatically resized with the window when the window changes. 
+ *                     The same is done with each image canvas and the display. Each canvas is binded to the display so each canvas is resized
+ *                     along with the display. 
+ */
 package jt_guevara;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -9,6 +53,13 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Gallery_Display {
+	private GridPane display;//gallery display component that contains image canvases
+	private StackPane leftCanvas = new StackPane();//image canvases
+	private StackPane midCanvas = new StackPane();
+	private StackPane rightCanvas = new StackPane();
+	
+	public Gallery_Display() {display = new GridPane();}//constructor
+	
 	public static void generate_gallery(Stage window,GridPane p)
 	{
 		GridPane galleryLayout = new GridPane();//gallery layout component(grid) for holding image canvases
