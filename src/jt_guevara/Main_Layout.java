904826981c0fcebe8121252f27a2cad35dfe1ecd
JTGuevara <84116849+JTGuevara@users.noreply.github.com>
2021-07-31 14:54:35 -0400

    Modified Main_Layout.java
    - Added new class and function info
    - Added new data fields: layout, MENU_FONT_SIZE
    - Added functions: Main_Layout()(constructor), getLayout()

--- src/jt_guevara/Main_Layout.java
+++ src/jt_guevara/Main_Layout.java
@@ -1,4 +1,21 @@
-
+/* FILE: Main_Layout.java
+ * DESCRIPTION: The Main_Layout class is used to set the style and layout of the application and menu items	
+ * 
+ * FUNCTIONS: 
+ *   public Main_Layout();
+ *      DESCRIPTION: Declares a Main_Layout by initializing a new layout(GridPane)
+ *      
+ *   public void setLayout();
+ *      DESCRIPTION: The application layout's style and general properties are set. 
+ *   
+ *   public GridPane getLayout();
+ *      DESCRIPTION: Returns layout
+ *                  
+ *   public void setMenuItems();
+ *      DESCRIPTION: Two text menu items are created, their style properties set and added to a menu bar. The menu bar is then added to
+ *              the main application layout
+ * 				
+ */
 package jt_guevara;
 
 import javafx.geometry.Insets;
@@ -9,6 +26,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main_Layout {
+	private GridPane layout;//main layout used for application
+	private final double MENU_FONT_SIZE = 12.4;//font size for menu items
+	public Main_Layout() {layout = new GridPane();}
+	public GridPane getLayout() {return layout;}
 	public static void setWindow(Stage window, GridPane p)
 	{
 		//Declare menu items to be used as clickable text for the user
