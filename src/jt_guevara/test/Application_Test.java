f0eb444420f3a9a84342b4d6d62b0f6544f9710b
Joan Torres <jtguevara585@hotmail.com>
2021-09-01 18:59:41 -0400

    Updated Application_Test.java
    Initial setup of test program.

--- src/jt_guevara/test/Application_Test.java
+++ src/jt_guevara/test/Application_Test.java
@@ -1,10 +1,56 @@
 package jt_guevara.test;
 
-public class Application_Test {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import jt_guevara.Button_Layout;
+import jt_guevara.Event_Handler;
+import jt_guevara.Gallery;
+import jt_guevara.Gallery_Display;
+import jt_guevara.Main_Layout;
+
+public class Application_Test extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		jt_guevara.Main.launch(args);
 	}
 
+	@Override
+	public void start(Stage window) throws Exception {
+		//START TEST PROGRAM	
+		
+				//declare application components
+				final String APPLICATION_NAME = "JT's Gallery";//application name
+			    final double MIN_WINDOW_WIDTH = 800.0;//minimum window width and height
+			    final double MIN_WINDOW_HEIGHT = 600.0;
+				Main_Layout mainLayout = new Main_Layout();//main user interface layout container for application
+				Scene s = new Scene(mainLayout.getLayout(),800,600);//required JavaFX structure to hold layout components
+				Gallery imageGallery = new Gallery();//collection class to hold images		
+				Gallery_Display display = new Gallery_Display();//layout sub-component for displaying images 
+				Button_Layout buttonLayout = new Button_Layout();//layout sub-component for manipulating images
+				Event_Handler handler = new Event_Handler();//class to set functionality to user interface components
+				
+				//set up components
+				mainLayout.setLayout();
+				mainLayout.set_menu_items();
+				display.setup_gallery();
+				display.bind_gallery(window);
+				buttonLayout.set_scroll_buttons();
+				buttonLayout.set_zoom_button();
+				buttonLayout.set_button_bar();
+				buttonLayout.add_buttons();
+				buttonLayout.bind_button_layout(window);
+				mainLayout.getLayout().add(display.getDisplay(), 0, 1);
+				mainLayout.getLayout().add(buttonLayout.getButtonBar(), 0, 2);
+				handler.load_event_handlers(window, mainLayout.getLayout(), imageGallery);
+				
+				//set up JavaFX stage
+				window.setTitle(APPLICATION_NAME);
+				window.setMinWidth(MIN_WINDOW_WIDTH);
+				window.setMinHeight(MIN_WINDOW_HEIGHT);
+				window.setScene(s);
+				
+				//show time!
+				window.show();
+	}
 }
