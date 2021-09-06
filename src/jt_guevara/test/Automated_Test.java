66ea7de593b610c9410ac423da8e8a9c3520d586
Joan Torres <jtguevara585@hotmail.com>
2021-09-06 15:57:44 -0400

    Updated Main_Test and Automated_Test
    Edited class info for both files. Moved test function info to Automated_Test.java

--- src/jt_guevara/test/Automated_Test.java
+++ src/jt_guevara/test/Automated_Test.java
@@ -1,3 +1,32 @@
+/* FILE: Automated_Test.java
+ * CLASS DESCRIPTION: Automated test class. The automated test will use a robot to simulate a user clicking on the user interface and testing the functionality
+ *     of each component of the image gallery(gallery upload, left-scroll, right-scroll, etc.). 
+ *     
+ * FUNCTIONS:
+ *	public void initialize_test(Robot testBot);
+ *      PARAMETERS: Robot testBot - robot class for automation
+ *                  Stage window - window for obtaining mouse coordinates relative to the user's monitor
+ *      DESCRIPTION: Starts the automated test. The function uses a robot to automate a user clicking a menu item to upload images and clicking
+ *         the lower buttons to manipulate images. The test consists of a series of animations where each mouse movement and click is recorded 
+ *         in a frame. The images used are sample images located in the project's directory. The initial test ends when the application is 
+ *         maximized to full-screen mode and the button test executes. 
+ *         
+ *         
+ *  public void moveMousePointer(Robot testBot, double x1, double x2, double y1, double y2);
+ *      PARAMETERS: Robot testBot - robot class for moving mouse pointer
+ *                  double x1,y1 - previous mouse coordinates
+ *                  double x2,y2 - destination mouse coordinates
+ *      DESCRIPTION: Automates a movement of the mouse pointer from point(x1,y1) to point(x2,y2) by moving the mouse pointer n times
+ *      
+ *  
+ *  public void button_test(Robot testBot, Stage window);
+ *      PARAMETERS: Robot testBot - robot class for automating button tests
+ *                  Stage window - window parameter for obtaining mouse coordinates relative to the user's monitor
+ *                  
+ *      DESCRIPTION: Starts automated button test. Uses a robot to simulate a user clicking on the button interface on the lower portion of 
+ *         the screen. The test consists of a series of animations where each mouse movement and click is recorded in a frame. The button 
+ *         test ends when the application is minimized and exited.
+ */
 package jt_guevara.test;
 
 public class Automated_Test {
--- src/jt_guevara/test/Main_Test.java
+++ src/jt_guevara/test/Main_Test.java
@@ -1,32 +1,5 @@
-/*FILE: Application_Test.java
- *DESCRIPTION: Main class for testing application. The Application_Test class will use automated testing to run the application.
- *     The automated test will simulate a user clicking on the user interface and testing the functionality of each component (gallery upload,
- *     left-scroll, right-scroll, etc.). The Application_Test class also serves as the application's second entry point.
- *     
- *FUNCTIONS:
- *	public void initialize_test(Robot testBot);
- *      PARAMETERS: Robot testBot - robot class for automation
- *                  Stage window - window for obtaining mouse coordinates relative to the user's monitor
- *      DESCRIPTION: Starts the automated test. The function uses a robot to automate a user clicking a menu item to upload images and clicking
- *         the lower buttons to manipulate images. The test consists of a series of animations where each mouse movement and click is recorded 
- *         in a frame. The images used are sample images located in the project's directory. The initial test ends when the application is 
- *         maximized to full-screen mode and the button test executes. 
- *         
- *         
- *  public void moveMousePointer(Robot testBot, double x1, double x2, double y1, double y2);
- *      PARAMETERS: Robot testBot - robot class for moving mouse pointer
- *                  double x1,y1 - previous mouse coordinates
- *                  double x2,y2 - destination mouse coordinates
- *      DESCRIPTION: Automates a movement of the mouse pointer from point(x1,y1) to point(x2,y2) by moving the mouse pointer n times
- *      
- *  
- *  public void button_test(Robot testBot, Stage window);
- *      PARAMETERS: Robot testBot - robot class for automating button tests
- *                  Stage window - window parameter for obtaining mouse coordinates relative to the user's monitor
- *                  
- *      DESCRIPTION: Starts automated button test. Uses a robot to simulate a user clicking on the button interface on the lower portion of 
- *         the screen. The test consists of a series of animations where each mouse movement and click is recorded in a frame. The button 
- *         test ends when the application is minimized and exited.
+/*FILE: Main_Test.java
+ *DESCRIPTION: Main class for testing application. The Main_Test class will run the application and initialize an automated test.
  */
 
 package jt_guevara.test;
