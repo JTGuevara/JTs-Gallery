1ab094953672b54f88352ff2a3359a962c85514f
Joan Torres <jtguevara585@hotmail.com>
2021-09-10 17:35:26 -0400

    Updated Automated_Test.java
    Added extra class info regarding the button test. Added member field to record test results.Edited comments for future changes.

--- src/jt_guevara/test/Automated_Test.java
+++ src/jt_guevara/test/Automated_Test.java
@@ -8,8 +8,8 @@
  *                  Stage window - window for obtaining mouse coordinates relative to the user's monitor
  *      DESCRIPTION: Starts the automated test. The function uses a robot to automate a user clicking a menu item to upload images and clicking
  *         the lower buttons to manipulate images. The test consists of a series of animations where each mouse movement and click is recorded 
- *         in a frame. The images used are sample images located in the project's directory. The initial test ends when the application is 
- *         maximized to full-screen mode and the button test executes. 
+ *         in a frame. The functionality of the menu items and gallery upload are tested and their results recorded. The function exits 
+ *         when the button test begins.
  *         
  *         
  *  public void moveMousePointer(Robot testBot, double x1, double x2, double y1, double y2);
@@ -19,13 +19,23 @@
  *      DESCRIPTION: Automates a movement of the mouse pointer from point(x1,y1) to point(x2,y2) by moving the mouse pointer n times
  *      
  *  
- *  public void button_test(Robot testBot, Stage window);
+ *  private void button_test(Robot testBot, Stage window);
  *      PARAMETERS: Robot testBot - robot class for automating button tests
  *                  Stage window - window parameter for obtaining mouse coordinates relative to the user's monitor
  *                  
  *      DESCRIPTION: Starts automated button test. Uses a robot to simulate a user clicking on the button interface on the lower portion of 
  *         the screen. The test consists of a series of animations where each mouse movement and click is recorded in a frame. The button 
- *         test ends when the application is minimized and exited.
+ *         test ends when the application is minimized from full-screen and exited.
+ *         
+ *   ------------------     
+ *        (NOTES):
+ *   ------------------
+ *      - The button test runs in full-screen mode, which may or may not work depending on your specific resolution. If that is the case, feel
+ *        free to adjust the robot's mouse pointer coordinates at each animation frame sequence while keeping the order of the animation intact.
+ *        
+ *      - Make sure to call the Stage.getX() and Stage.getY() methods to get the window position of the application and the Robot.getMouseX() 
+ *        and Robot.getMouseY() methods to get its current mouse coordinates. These will aid in getting the correct (x,y) coordinates for your specific
+ *        machine so the robot can locate and activate the user interface components.
  */
 package jt_guevara.test;
 
@@ -33,19 +43,24 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.HBox;
 import javafx.scene.robot.Robot;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import jt_guevara.Gallery_Display;
 
 public class Automated_Test {
-	public void initialize_test(Robot testBot, Stage window) throws InterruptedException {
+	public Automated_Test() {}//constructor
+	public String result = "";//string to hold test results
+	
+	public void initialize_test(Robot testBot, Stage window, HBox menuBar, Gallery_Display display) throws InterruptedException {
 		double x = testBot.getMouseX();//get current mouse coordinates
 		double y = testBot.getMouseY();
-		Timeline t = new Timeline( //execute animation (
+		Timeline t = new Timeline( //define animation 
 			new KeyFrame(Duration.ZERO, event -> {moveMousePointer(testBot,x,x,y,y);}),
 			new KeyFrame(Duration.seconds(0.5), event -> {moveMousePointer(testBot,x,x + 30,y,y + 50);}), //upload gallery 
-			new KeyFrame(Duration.seconds(1), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
-			new KeyFrame(Duration.seconds(1.5), event -> {moveMousePointer(testBot,x + 20,x + 150,y + 50,y + 250);}),//click on images in file-chooser
+			new KeyFrame(Duration.seconds(1), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),//gallery menu item test
+			new KeyFrame(Duration.seconds(1.5), event -> {moveMousePointer(testBot,x + 20,x + 150,y + 50,y + 250);}),
 			new KeyFrame(Duration.seconds(2), event -> {testBot.keyPress(KeyCode.CONTROL);}),
 			new KeyFrame(Duration.seconds(2), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
 			new KeyFrame(Duration.seconds(2.3), event -> {moveMousePointer(testBot,x + 150, x + 300,y + 250, y + 250);}),
@@ -53,9 +68,9 @@ public class Automated_Test {
 			new KeyFrame(Duration.seconds(2.9), event -> {moveMousePointer(testBot,x + 300, x + 450,y + 250, y + 250);}),
 			new KeyFrame(Duration.seconds(3.3), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
 			new KeyFrame(Duration.seconds(3.5), event -> {testBot.keyRelease(KeyCode.CONTROL);}),
-			new KeyFrame(Duration.seconds(3.9), event -> {moveMousePointer(testBot,x + 450, x + 800,y + 250, y + 580);}),//click open to close file
-			new KeyFrame(Duration.seconds(4), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
-			new KeyFrame(Duration.seconds(4.5), event -> {button_test(testBot, window);}) //starts button test
+			new KeyFrame(Duration.seconds(3.9), event -> {moveMousePointer(testBot,x + 450, x + 800,y + 250, y + 580);}),//click open to close file dialog
+			new KeyFrame(Duration.seconds(4), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),//gallery upload test
+			new KeyFrame(Duration.seconds(4.5), event -> {button_test(testBot, window);}) //button test
 			);
 		t.setCycleCount(1);
 		t.play();
@@ -70,15 +85,15 @@ public class Automated_Test {
 			testBot.mouseMove(x1 + dx * i, y1 + dy * i);
 	}
 	
-	public void button_test(Robot testBot, Stage window) {
+	private void button_test(Robot testBot, Stage window) {
 		double x = testBot.getMouseX();//get current mouse coordinates
 		double y = testBot.getMouseY();
 		
-		Timeline t = new Timeline( //execute animation 
+		Timeline t = new Timeline( //define animation 
 			new KeyFrame(Duration.ZERO, event -> {moveMousePointer(testBot,x,x,y,y);}),	
-			new KeyFrame(Duration.seconds(0.5), event -> {moveMousePointer(testBot,x,x - 50,y,y - 570);}),//navigate to maximize to full-screen
-			new KeyFrame(Duration.seconds(0.9), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
-			new KeyFrame(Duration.seconds(1.7), event -> {moveMousePointer(testBot,x - 50,window.getWidth() / 2,y - 570,window.getHeight() / 1.2);}),//navigate to buttons
+			new KeyFrame(Duration.seconds(0.7), event -> {moveMousePointer(testBot,x,x - 50,y,y - 570);}),//navigate to maximize icon for full-screen
+			new KeyFrame(Duration.seconds(1.1), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
+			new KeyFrame(Duration.seconds(1.7), event -> {moveMousePointer(testBot,x - 50,window.getWidth() / 2,y - 570,window.getHeight() / 1.2);}),//navigate to zoom button
 			new KeyFrame(Duration.seconds(2.3), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
 			new KeyFrame(Duration.seconds(2.9), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
 			new KeyFrame(Duration.seconds(3.5), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
@@ -94,9 +109,13 @@ public class Automated_Test {
 			new KeyFrame(Duration.seconds(9.7), event -> {moveMousePointer(testBot,testBot.getMouseX() - 230,window.getWidth() - 100,y + 160,10);}),//navigate to minimize screen
 			new KeyFrame(Duration.seconds(9.9), event -> {testBot.mouseClick(MouseButton.PRIMARY);}),
 			new KeyFrame(Duration.seconds(10.4), event -> {moveMousePointer(testBot,testBot.getMouseX() - 100,window.getX() + 70, window.getY(),window.getY() + 50);}),//navigate to exit
-			new KeyFrame(Duration.seconds(10.8), event -> {testBot.mouseClick(MouseButton.PRIMARY);})
+			new KeyFrame(Duration.seconds(10.8), event -> {System.out.println("RESULTS:\n" + "---------\n\n" + result);}),//print test result and close program
+			new KeyFrame(Duration.seconds(11), event -> {testBot.mouseClick(MouseButton.PRIMARY);})
+			
 			);
 		t.setCycleCount(1);
 		t.play();
 	}
+	
+	
 }
