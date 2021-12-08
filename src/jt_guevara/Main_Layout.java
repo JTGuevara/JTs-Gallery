92626d1335aae5cf2695195b0bee1d0d21daf26b
Joan Torres <jtguevara585@hotmail.com>
2021-12-07 22:45:02 -0500

    Updated file for refactoring
    (Main_Layout.java) Added new functions and documentation, updated existing functions

--- src/jt_guevara/Main_Layout.java
+++ src/jt_guevara/Main_Layout.java
@@ -80,8 +80,14 @@ public class Main_Layout {
 		layout.add(menuBar, 0, 0);
 	}
 	
+	/*
+	 private static Stage generateSettingsWindow(Stage window);
+	   PARAMETERS: Stage window - Parent window required for creating a child window of application settings
+	   DESCRIPTION: An application settings window is created, along with settings sub-components for adjusting application settings, and returned. 
+	 */
+	
 	private static Stage generateSettingsWindow(Stage window) {
-		//DECLARE WINDOW AND SETTING COMPONENTS 
+		//DECLARE SETTINGS WINDOW AND ITS COMPONENTS 
 		Stage settings = new Stage();
 		Pane p = new Pane();
 		Scene s = new Scene(p,500,280);
@@ -98,8 +104,38 @@ public class Main_Layout {
 		ColorPicker colorPicker = new ColorPicker();//color picker objects for applying background settings to layout components
 		ColorPicker colorPicker2 = new ColorPicker();
 		ColorPicker colorPicker3 = new ColorPicker();
-		//APPLY SETTING COMPONENTS TO WINDOW
-		//(set buttons)
+		//STYLE SETTINGS WINDOW COMPONENTS
+		styleSettingWindowText(bgSettingsTable,bgDescription,bgSetting,bgSetting2,bgSetting3);
+		styleSettingsButtons(OK, cancel);
+		styleColorPickers(colorPicker);
+		applySettingsButtons(settings, OK, cancel, layout, display, buttonLayout);
+		//APPLY SETTINGS 
+		applySettings(colorPicker, colorPicker2, colorPicker3, layout, display, buttonLayout);
+		//ADD SETTINGS COMPONENTS TO WINDOW
+		p.setPadding(new Insets(10,10,10,10));
+		p.setStyle("-fx-background-color: black");
+		bgSettingsTable.add(bgSetting, 0, 0);
+		bgSettingsTable.add(bgSetting2, 0, 1);
+		bgSettingsTable.add(bgSetting3, 0, 2);
+		bgSettingsTable.add(colorPicker, 1, 0);
+		bgSettingsTable.add(colorPicker2, 1, 1);
+		bgSettingsTable.add(colorPicker3, 1, 2);
+		p.getChildren().addAll(bgDescription, bgSettingsTable, OK, cancel);
+		//SET UP WINDOW
+		settings.setTitle("Settings");
+		settings.setWidth(500);
+		settings.setHeight(280);
+		settings.setScene(s);
+		return settings;
+	}
+	
+	/*
+	 private static void styleSettingsButtons(Button OK, Button cancel);
+	    PARAMETERS: Button OK, cancel - buttons for applying style properties
+	    DESCRIPTION: The OK and Cancel of the 'Settings' window have their style properties set
+	*/
+	
+	private static void styleSettingsButtons(Button OK, Button cancel) {
 		OK.setLayoutX(180);
 		OK.setLayoutY(200);
 		OK.setStyle("-fx-background-color: black;-fx-border-color: white;-fx-border-width: 1px");
@@ -110,8 +146,17 @@ public class Main_Layout {
 		cancel.setStyle("-fx-background-color: black;-fx-border-color: white;-fx-border-width: 1px");
 		cancel.setText("Cancel");
 		cancel.setTextFill(Color.WHITE);
-		setWindowButtons(settings, OK, cancel, layout, display, buttonLayout);
-		//(set text descriptions)
+	}
+	
+	/*
+	 private static void styleSettingWindowText(GridPane bgSettingsTable, Text bgDescription, Text bgSetting, Text bgSetting2, Text bgSetting3);
+	    PARAMETERS: GridPane bgSettingsTable - 
+	                Text bgDescription, bgSetting, - text descriptions for background settings
+	                     bgSetting2, bgSetting3 
+	    DESCRIPTION: Each text component in the 'Settings' window has its style properties set
+	*/
+	
+	private static void styleSettingWindowText(GridPane bgSettingsTable, Text bgDescription, Text bgSetting, Text bgSetting2, Text bgSetting3) {
 		bgDescription.setFont(Font.font(20));
 		bgDescription.setFill(Color.WHITE);
 		bgDescription.setLayoutX(20);
@@ -126,40 +171,16 @@ public class Main_Layout {
 		bgSetting2.setFill(Color.WHITE);
 		bgSetting3.setFont(Font.font(17));
 		bgSetting3.setFill(Color.WHITE);
-		colorPicker.setLayoutX(300);
-		colorPicker.setLayoutY(70);
-		/*apply background settings using color pickers to change background color of 
-		layout components*/
-		colorPicker.setOnAction(event->{
-			//apply background settings to main layout
-			String color = colorPicker.getValue().toString();
-			layout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";");
-			});
-		colorPicker2.setOnAction(event->{
-			//apply background settings to display
-			String color = colorPicker2.getValue().toString();
-			display.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
-			});
-		colorPicker3.setOnAction(event->{
-			//apply background settings to button layout
-			String color = colorPicker3.getValue().toString();
-			buttonLayout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
-			});
-		//SET UP WINDOW 
-		settings.setTitle("Settings");
-		settings.setWidth(500);
-		settings.setHeight(280);
-		settings.setScene(s);
-		p.setPadding(new Insets(10,10,10,10));
-		p.setStyle("-fx-background-color: black");
-		bgSettingsTable.add(bgSetting, 0, 0);
-		bgSettingsTable.add(bgSetting2, 0, 1);
-		bgSettingsTable.add(bgSetting3, 0, 2);
-		bgSettingsTable.add(colorPicker, 1, 0);
-		bgSettingsTable.add(colorPicker2, 1, 1);
-		bgSettingsTable.add(colorPicker3, 1, 2);
-		p.getChildren().addAll(bgDescription, bgSettingsTable, OK, cancel);
-		return settings;
+	}
+	
+	/*
+	  private static void styleColorPickers();
+	    PARAMETERS:
+	    DESCRIPTION:
+	*/
+	private static void styleColorPickers(ColorPicker picker) {
+		picker.setLayoutX(300);
+		picker.setLayoutY(70);
 	}
 	
 	/*
@@ -191,5 +212,34 @@ public class Main_Layout {
 					+ "-fx-border-width: 5px;-fx-background-color: darkblue;");
 			settings.close();
 		});
+	}
+	
+	/*
+	 private static void applySettings(ColorPicker color1,ColorPicker color2, ColorPicker color3, GridPane layout, GridPane display, GridPane buttonLayout);
+	   PARAMETERS: ColorPicker color1, color2, color3 - components for applying background settings to layout components
+	               GridPane layout - main layout for applying background settings
+	               GridPane display - gallery display for applying background settings
+	               GridPane buttonLayout - button interface for applying background settings
+	   DESCRIPTION: Background color settings are applied to application components.
+	*/
+	private static void applySettings(ColorPicker color1, ColorPicker color2, ColorPicker color3, GridPane layout, GridPane display,GridPane buttonLayout)
+	{
+		/*apply background settings using color pickers to change background color of 
+		layout components*/
+		color1.setOnAction(event->{
+			//apply background settings to main layout
+			String color = color1.getValue().toString();
+			layout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";");
+			});
+		color2.setOnAction(event->{
+			//apply background settings to display
+			String color = color2.getValue().toString();
+			display.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
+			});
+		color3.setOnAction(event->{
+			//apply background settings to button layout
+			String color = color3.getValue().toString();
+			buttonLayout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
+			});
 	}
 }
