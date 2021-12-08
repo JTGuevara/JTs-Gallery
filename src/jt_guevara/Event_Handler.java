b4af78e5c7f0d68239489d637dcc122c3cc7e416
Joan Torres <jtguevara585@hotmail.com>
2021-12-07 19:27:33 -0500

    Updated files for refactoring
    (Event_Handler.java)(Main_Layout.java) Moved all settings window functionality to Main_Layout.java

--- src/jt_guevara/Event_Handler.java
+++ src/jt_guevara/Event_Handler.java
@@ -1,24 +1,6 @@
 /* FILE: Event_Handler.java
- * DESCRIPTION: 
- *    The Event_Handler class handles the overall functionality and behavior of the application. The class sets event handlers(functions) to user interface 
- *    components(gallery display, menu components, buttons) that are called in response to the user's actions 
- * 
- * FUNCTIONS:
- *    
- *                     
- *    
- *    private static void applySettingsButtons(Stage settings, Button OK, Button cancel, GridPane layout, GridPane display, GridPane buttonLayout);
- *       PARAMETERS: Stage settings - settings window for setting 'OK' and 'Cancel' buttons
- *                   Button OK - OK button for confirming new application settings
- *                   Button cancel - Cancel button for modifying application settings to default 
- *                   GridPane layout - layout component for modifying settings to default after cancellation
- *                   GridPane display - gallery display component for modifying settings to default after cancellation
- *                   GridPane buttonLayout - button layout component for modifying settings to default after cancellation
- *                   
- *       DESCRIPTION: Sets the actions of the application setting window's 'OK' and 'Cancel' buttons. The OK button is set to close the settings window
- *                    and confirm any application settings that are made. The 'Cancel' button resets any application settings to default and then closes
- *                    the window.
- * 		                         					 
+ * DESCRIPTION: The Event_Handler class handles the overall functionality and behavior of the application. The class sets event handlers(functions) to 
+ *              user interface components(gallery display, menu components, buttons) that are called in response to the user's actions  		                         					 
  */
 
 package jt_guevara;
@@ -155,105 +137,6 @@ public class Event_Handler {
 			}
 	}
 	
-	private static Stage applySettings(Stage window) {
-		//DECLARE WINDOW AND SETTING COMPONENTS 
-		Stage settings = new Stage();
-		Pane p = new Pane();
-		Scene s = new Scene(p,500,280);
-		Button OK = new Button();//setting window buttons
-		Button cancel = new Button();
-		GridPane layout = (GridPane) window.getScene().getRoot();//retrieve layout components for applying background settings
-		GridPane display = (GridPane) layout.getChildren().get(1);
-		GridPane buttonLayout = (GridPane) layout.getChildren().get(2);
-		GridPane bgSettingsTable = new GridPane();//container for background settings
-		Text bgDescription = new Text("Change Background:");//text descriptions for background settings
-		Text bgSetting = new Text("Main: ");
-		Text bgSetting2 = new Text("Gallery Interface: ");
-		Text bgSetting3 = new Text("Button Interface: ");
-		ColorPicker colorPicker = new ColorPicker();//color picker objects for applying background settings to layout components
-		ColorPicker colorPicker2 = new ColorPicker();
-		ColorPicker colorPicker3 = new ColorPicker();
-		//APPLY SETTING COMPONENTS TO WINDOW
-		//(set buttons)
-		OK.setLayoutX(180);
-		OK.setLayoutY(200);
-		OK.setStyle("-fx-background-color: black;-fx-border-color: white;-fx-border-width: 1px");
-		OK.setText("OK");
-		OK.setTextFill(Color.WHITE);
-		cancel.setLayoutX(240);
-		cancel.setLayoutY(200);
-		cancel.setStyle("-fx-background-color: black;-fx-border-color: white;-fx-border-width: 1px");
-		cancel.setText("Cancel");
-		cancel.setTextFill(Color.WHITE);
-		setWindowButtons(settings, OK, cancel, layout, display, buttonLayout);
-		//(set text descriptions)
-		bgDescription.setFont(Font.font(20));
-		bgDescription.setFill(Color.WHITE);
-		bgDescription.setLayoutX(20);
-		bgDescription.setLayoutY(50);
-		bgSettingsTable.setVgap(10);
-		bgSettingsTable.setHgap(50);
-		bgSettingsTable.setLayoutX(50);
-		bgSettingsTable.setLayoutY(75);
-		bgSetting.setFont(Font.font(17));
-		bgSetting.setFill(Color.WHITE);
-		bgSetting2.setFont(Font.font(17));
-		bgSetting2.setFill(Color.WHITE);
-		bgSetting3.setFont(Font.font(17));
-		bgSetting3.setFill(Color.WHITE);
-		colorPicker.setLayoutX(300);
-		colorPicker.setLayoutY(70);
-		/*apply background settings using color pickers to change background color of 
-		layout components*/
-		colorPicker.setOnAction(event->{
-			//apply background settings to main layout
-			String color = colorPicker.getValue().toString();
-			layout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";");
-			});
-		colorPicker2.setOnAction(event->{
-			//apply background settings to display
-			String color = colorPicker2.getValue().toString();
-			display.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
-			});
-		colorPicker3.setOnAction(event->{
-			//apply background settings to button layout
-			String color = colorPicker3.getValue().toString();
-			buttonLayout.setStyle("-fx-background-color: #" + color.substring(2,8) + ";-fx-border-radius: 10px;-fx-background-radius: 10px");
-			});
-		//SET UP WINDOW 
-		settings.setTitle("Settings");
-		settings.setWidth(500);
-		settings.setHeight(280);
-		settings.setScene(s);
-		p.setPadding(new Insets(10,10,10,10));
-		p.setStyle("-fx-background-color: black");
-		bgSettingsTable.add(bgSetting, 0, 0);
-		bgSettingsTable.add(bgSetting2, 0, 1);
-		bgSettingsTable.add(bgSetting3, 0, 2);
-		bgSettingsTable.add(colorPicker, 1, 0);
-		bgSettingsTable.add(colorPicker2, 1, 1);
-		bgSettingsTable.add(colorPicker3, 1, 2);
-		p.getChildren().addAll(bgDescription, bgSettingsTable, OK, cancel);
-		return settings;
-	}
-	
-	private static void setWindowButtons(Stage settings, Button OK, Button cancel, GridPane layout, GridPane display, GridPane buttonLayout) {
-		//OK button - confirm any new application settings and close window
-		//Cancel button - set application settings to default and close window
-		OK.setOnMouseClicked(event->{
-			settings.close();
-		});
-		cancel.setOnMouseClicked(event->{
-			//reset background settings to default
-			layout.setStyle("-fx-background-color: black");
-			display.setStyle("-fx-border-color: blue;-fx-border-width: 5px;-fx-border-color: royalblue;-fx-border-radius: 10px;"
-					+ "-fx-background-color: darkblue;-fx-background-radius: 10px");
-			buttonLayout.setStyle("-fx-border-color: royalblue;-fx-border-radius: 10px;-fx-background-radius: 10px;"
-					+ "-fx-border-width: 5px;-fx-background-color: darkblue;");
-			settings.close();
-		});
-	}
-
 	/*
 	  private static void setButtons(GridPane layout, ArrayDeque<Image> imageGallery, StackPane midCanvas, ImageView leftImgView, ImageView midImgView, 
                                      ImageView rightImgView);
