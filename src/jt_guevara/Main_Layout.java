134f59b79ba89c2f69f486fddf4a0c1998f1f058
Joan Torres <jtguevara585@hotmail.com>
2022-02-07 02:13:39 -0500

    Edited and formatted documentation
    (Main_Layout.java)(Gallery_Display.java) Edited and formatted documentation.

--- src/jt_guevara/Main_Layout.java
+++ src/jt_guevara/Main_Layout.java
@@ -12,15 +12,15 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Main_Layout {
-	public Main_Layout() {layout = new GridPane();}//constructor
-	public GridPane layout;//main layout used for application
-	final private Text gallery = new Text("Gallery");//menu items
+	public Main_Layout() {layout = new GridPane();}			//constructor
+	public GridPane layout;									//main layout used for application
+	final private Text gallery = new Text("Gallery");		//menu items(text)
 	final private Text settings = new Text("Settings");
 	final private Text exit = new Text("Exit");
-	private HBox menuBar = new HBox();//menu bar to store menu items
-	public final double MENU_FONT_SIZE = 12.399;//font size for menu items
+	private HBox menuBar = new HBox();						//menu bar to store menu items
+	private final double MENU_FONT_SIZE = 12.399;			//font size for menu items
 	
-	public HBox getMenuBar() {return menuBar;}
+	public HBox getMenuBar() {return menuBar;}				//get functions for menu bar and items
 	public Text getMenuItem1() {return gallery;}
 	public Text getMenuItem2() {return settings;}
 	public Text getMenuItem3() {return exit;}
@@ -37,19 +37,21 @@ public class Main_Layout {
 	}
 	
 /*
-  public void setMenuItems();
+  public void setMenuComponents();
      DESCRIPTION: Two text menu items are created, their style properties set and added to a menu bar. The menu bar is then added to
                   the main application layout 
 */
-	public void set_menu_items(){
-		//set menu items to be used as clickable text for the user
+	public void setMenuComponents(){
+		//To set menu items:
+		//1.) apply style properties to each menu item and menu bar
+		//2.) add menu items to menu bar
+		//3.) add menu bar to the main layout
 		gallery.setFill(Color.WHITE);
 		gallery.setFont(Font.font(MENU_FONT_SIZE));
 		settings.setFill(Color.WHITE);
 		settings.setFont(Font.font(MENU_FONT_SIZE));
 		exit.setFill(Color.WHITE);
 		exit.setFont(Font.font(MENU_FONT_SIZE));
-		//set menu bar properties
 		menuBar.setSpacing(10.0);
 		menuBar.getChildren().add(gallery);
 		menuBar.getChildren().add(settings);
